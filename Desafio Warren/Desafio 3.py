75d29a35fdc0ff80f48039c9b2e25e6ac26c3ce7
MatheusPitsch <matheuspitschh@gmail.com>
2022-04-29 23:20:36 -0300

    desafio_warren

--- Desafio Warren/Desafio 3.py
+++ Desafio Warren/Desafio 3.py
@@ -1,1 +1,31 @@
 ''''''
+from random import Random
+
+import numpy
+
+
+vetor = []
+soma = 0
+j= 0
+
+valor = int(input('Digite um valor: '))
+
+for i in range(3):
+    valor_soma = int(input('Digite um valor: '))
+    vetor.append(valor_soma)
+
+while True:
+    
+    vetor_aleatorio = []
+
+    for i in range(3):
+        valor_aleatorio = numpy.random.choice(vetor)
+        vetor_aleatorio.append(valor_aleatorio)
+    
+    for i in vetor_aleatorio:
+        if soma == valor:
+            print(vetor_aleatorio)
+            j += 1
+    
+    if j == 10:
+        break
